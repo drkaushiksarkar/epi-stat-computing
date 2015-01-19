64ff988eb9b048ec379db089065651006ad6bccf
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-01-19 19:58:10 +0000

    feat(schemas): cohort_study schemas v2

---
+++ src/schemas/cohort_study_v2_d4655_c28.py
@@ -0,0 +1,21 @@
+"""CohortStudy schemas v2d4655y2015."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class CohortStudyConfig_v2d4655y2015:
+    enabled: bool = True
+    batch_size: int = 64
+    hidden_dim: int = 128
+    num_layers: int = 4
+    dropout: float = 0.2
+    learning_rate: float = 2.0e-04
+    max_epochs: int = 20
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "CohortStudyConfig_v2d4655y2015":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
