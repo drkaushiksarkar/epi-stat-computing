a40e21e42182d7ad4aa5ce288237f34bec4f35a1
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-02-02 22:56:51 +0000

    perf(tests): cohort_study tests v7

---
+++ src/tests/cohort_study_v7_d2211_c60.py
@@ -0,0 +1,23 @@
+"""Tests for cohort_study v7d2211y2015."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestCohortStudyV7D2211Y2015(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "cohort_study", "variant": 7}
+        self.assertEqual(params["variant"], 7)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 700)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(8, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
