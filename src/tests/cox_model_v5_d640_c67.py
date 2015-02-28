347616c9de9471cf02676d674344e57e2f7f0d79
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-02-28 20:31:55 +0000

    perf(tests): cox_model tests v5

---
+++ src/tests/cox_model_v5_d640_c67.py
@@ -0,0 +1,23 @@
+"""Tests for cox_model v5d640y2015."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestCoxModelV5D640Y2015(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "cox_model", "variant": 5}
+        self.assertEqual(params["variant"], 5)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 500)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(6, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
