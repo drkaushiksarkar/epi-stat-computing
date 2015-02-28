5be402ecd301815a2964ffc14f8069221f38a2a2
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-02-28 21:38:36 +0000

    docs(tests): spatial_stats tests v1

---
+++ src/tests/spatial_stats_v1_d640_c117.py
@@ -0,0 +1,23 @@
+"""Tests for spatial_stats v1d640y2015."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestSpatialStatsV1D640Y2015(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "spatial_stats", "variant": 1}
+        self.assertEqual(params["variant"], 1)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 100)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(2, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
