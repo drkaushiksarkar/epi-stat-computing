459801840ec411cc76d98bea87238a698b24f4a3
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-03-10 07:59:46 +0000

    test(tests): cohort_study tests v9

---
+++ src/tests/cohort_study_v9_d1291_c80.py
@@ -0,0 +1,23 @@
+"""Tests for cohort_study v9d1291y2015."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestCohortStudyV9D1291Y2015(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "cohort_study", "variant": 9}
+        self.assertEqual(params["variant"], 9)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 900)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(10, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
