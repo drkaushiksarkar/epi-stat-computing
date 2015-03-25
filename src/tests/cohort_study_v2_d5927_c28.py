494db0a1f70f6ab3995b60265b2c6cb460a43cdf
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-03-25 17:29:16 +0000

    feat(tests): cohort_study tests v2

---
+++ src/tests/cohort_study_v2_d5927_c28.py
@@ -0,0 +1,23 @@
+"""Tests for cohort_study v2d5927y2015."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestCohortStudyV2D5927Y2015(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "cohort_study", "variant": 2}
+        self.assertEqual(params["variant"], 2)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 200)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(3, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
