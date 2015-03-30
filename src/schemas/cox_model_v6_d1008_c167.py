840d460fb7d4d6581b53bfdd080a5df6e0749386
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-03-30 19:28:13 +0000

    chore(schemas): cox_model schemas v6

---
+++ src/schemas/cox_model_v6_d1008_c167.py
@@ -0,0 +1,21 @@
+"""CoxModel schemas v6d1008y2015."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class CoxModelConfig_v6d1008y2015:
+    enabled: bool = True
+    batch_size: int = 192
+    hidden_dim: int = 384
+    num_layers: int = 8
+    dropout: float = 0.6
+    learning_rate: float = 6.0e-04
+    max_epochs: int = 60
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "CoxModelConfig_v6d1008y2015":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
