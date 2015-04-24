fb9eb7aa9c50e4d940e32fbec2e0c9068c2ca157
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-04-24 22:00:26 +0000

    perf(config): permutation_test config v4

---
+++ src/config/permutation_test_v4_d3867_c102.py
@@ -0,0 +1,21 @@
+"""PermutationTest config v4d3867y2015."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class PermutationTestConfig_v4d3867y2015:
+    enabled: bool = True
+    batch_size: int = 128
+    hidden_dim: int = 256
+    num_layers: int = 6
+    dropout: float = 0.4
+    learning_rate: float = 4.0e-04
+    max_epochs: int = 40
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "PermutationTestConfig_v4d3867y2015":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
