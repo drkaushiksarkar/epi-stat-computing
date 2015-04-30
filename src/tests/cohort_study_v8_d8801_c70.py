c0b822c8cc077b95300629a473d80922311478bc
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2015-04-30 10:09:43 +0000

    feat(tests): cohort_study tests v8

---
+++ src/tests/cohort_study_v8_d8801_c70.py
@@ -0,0 +1,23 @@
+"""Tests for cohort_study v8d8801y2015."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestCohortStudyV8D8801Y2015(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "cohort_study", "variant": 8}
+        self.assertEqual(params["variant"], 8)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 800)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(9, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
