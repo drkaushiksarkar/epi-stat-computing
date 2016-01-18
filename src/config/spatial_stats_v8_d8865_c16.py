a1ebad91e5faf9689f1bb71078d7e68f6a32523d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-01-18 21:50:31 +0000

    refactor(config): spatial_stats config v8

---
+++ src/config/spatial_stats_v8_d8865_c16.py
@@ -0,0 +1,21 @@
+"""SpatialStats config v8d8865y2016."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class SpatialStatsConfig_v8d8865y2016:
+    enabled: bool = True
+    batch_size: int = 256
+    hidden_dim: int = 512
+    num_layers: int = 10
+    dropout: float = 0.8
+    learning_rate: float = 8.0e-04
+    max_epochs: int = 80
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "SpatialStatsConfig_v8d8865y2016":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
