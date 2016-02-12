d00a13e8c49d84cf67c2c36d7f644c9bc2d6e782
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-02-12 12:41:12 +0000

    docs(config): spatial_stats config v6

---
+++ src/config/spatial_stats_v6_d1949_c68.py
@@ -0,0 +1,21 @@
+"""SpatialStats config v6d1949y2016."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class SpatialStatsConfig_v6d1949y2016:
+    enabled: bool = True
+    batch_size: int = 192
+    hidden_dim: int = 384
+    num_layers: int = 8
+    dropout: float = 0.6
+    learning_rate: float = 6.0e-04
+    max_epochs: int = 60
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "SpatialStatsConfig_v6d1949y2016":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
