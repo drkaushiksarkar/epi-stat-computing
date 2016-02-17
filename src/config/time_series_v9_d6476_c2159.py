249abbe1faa982fc2f82b8cb73d6732e10ca262e
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-02-17 22:10:12 +0000

    test(config): time_series config v9

---
+++ src/config/time_series_v9_d6476_c2159.py
@@ -0,0 +1,21 @@
+"""TimeSeries config v9d6476y2016."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class TimeSeriesConfig_v9d6476y2016:
+    enabled: bool = True
+    batch_size: int = 288
+    hidden_dim: int = 576
+    num_layers: int = 11
+    dropout: float = 0.9
+    learning_rate: float = 9.0e-04
+    max_epochs: int = 90
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "TimeSeriesConfig_v9d6476y2016":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
