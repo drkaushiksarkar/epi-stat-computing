793a843fff44117e8be18eec1ae0d0fb8c3ea5a7
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 14:15:40 +0000

    fix(schemas): cohort_study schemas v7

--- src/schemas/cohort_study_v7_d4644_c15.py
+++ src/schemas/cohort_study_v7_d4644_c15.py
@@ -1,10 +1,10 @@
-"""CohortStudy schemas v7d4644y2015."""
+"""CohortStudy schemas v7d4644y2016."""
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
 
 @dataclass
-class CohortStudyConfig_v7d4644y2015:
+class CohortStudyConfig_v7d4644y2016:
     enabled: bool = True
     batch_size: int = 224
     hidden_dim: int = 448
@@ -14,7 +14,7 @@ class CohortStudyConfig_v7d4644y2015:
     max_epochs: int = 70
 
     @classmethod
-    def from_dict(cls, d: Dict[str, Any]) -> "CohortStudyConfig_v7d4644y2015":
+    def from_dict(cls, d: Dict[str, Any]) -> "CohortStudyConfig_v7d4644y2016":
         return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
 
     def validate(self) -> bool:
