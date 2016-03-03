e60d2359524ff4acb6e405fc24d0944f9a9a6523
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 22:16:06 +0000

    test(services): regression services v4

--- src/services/regression_v4_d4644_c66.py
+++ src/services/regression_v4_d4644_c66.py
@@ -1,4 +1,4 @@
-"""Regression services v4d4644y2015.
+"""Regression services v4d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class Regression_v4d4644y2015:
+class Regression_v4d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 400):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
