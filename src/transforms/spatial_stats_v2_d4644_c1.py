76efba0a0d54da7342dfa665fddac3cfc629877a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 07:44:17 +0000

    fix(transforms): spatial_stats transforms v2

--- src/transforms/spatial_stats_v2_d4644_c1.py
+++ src/transforms/spatial_stats_v2_d4644_c1.py
@@ -1,4 +1,4 @@
-"""SpatialStats transforms v2d4644y2015.
+"""SpatialStats transforms v2d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class SpatialStats_v2d4644y2015:
+class SpatialStats_v2d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 200):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
