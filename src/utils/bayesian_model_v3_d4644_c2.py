b6318ef3b6fb271e428e1d94a0ee4ac3e5296810
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 10:57:16 +0000

    refactor(utils): bayesian_model utils v3

--- src/utils/bayesian_model_v3_d4644_c2.py
+++ src/utils/bayesian_model_v3_d4644_c2.py
@@ -1,4 +1,4 @@
-"""BayesianModel utils v3d4644y2015.
+"""BayesianModel utils v3d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class BayesianModel_v3d4644y2015:
+class BayesianModel_v3d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 300):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
