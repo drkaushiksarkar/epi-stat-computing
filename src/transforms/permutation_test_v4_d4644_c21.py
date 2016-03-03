61a4b8867b7d0e0501e883b0cd2716b56d633ba5
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 22:58:07 +0000

    feat(transforms): permutation_test transforms v4

--- src/transforms/permutation_test_v4_d4644_c21.py
+++ src/transforms/permutation_test_v4_d4644_c21.py
@@ -1,4 +1,4 @@
-"""PermutationTest transforms v4d4644y2015.
+"""PermutationTest transforms v4d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class PermutationTest_v4d4644y2015:
+class PermutationTest_v4d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 400):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
