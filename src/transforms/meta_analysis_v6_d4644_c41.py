9cc017fc0137a410eb8714d764056bb67a56c662
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 11:38:15 +0000

    chore(transforms): meta_analysis transforms v6

--- src/transforms/meta_analysis_v6_d4644_c41.py
+++ src/transforms/meta_analysis_v6_d4644_c41.py
@@ -1,4 +1,4 @@
-"""MetaAnalysis transforms v6d4644y2015.
+"""MetaAnalysis transforms v6d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class MetaAnalysis_v6d4644y2015:
+class MetaAnalysis_v6d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 600):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
