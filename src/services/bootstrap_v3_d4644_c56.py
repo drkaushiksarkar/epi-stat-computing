f1083422da484a4688e90779051d42c5b6ab9920
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 07:37:09 +0000

    feat(services): bootstrap services v3

--- src/services/bootstrap_v3_d4644_c56.py
+++ src/services/bootstrap_v3_d4644_c56.py
@@ -1,4 +1,4 @@
-"""Bootstrap services v3d4644y2015.
+"""Bootstrap services v3d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class Bootstrap_v3d4644y2015:
+class Bootstrap_v3d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 300):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
