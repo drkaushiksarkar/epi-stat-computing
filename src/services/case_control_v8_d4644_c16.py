f95a4f7a6845586b450a08dab16ba9bb9de22c79
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 17:06:30 +0000

    refactor(services): case_control services v8

--- src/services/case_control_v8_d4644_c16.py
+++ src/services/case_control_v8_d4644_c16.py
@@ -1,4 +1,4 @@
-"""CaseControl services v8d4644y2015.
+"""CaseControl services v8d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class CaseControl_v8d4644y2015:
+class CaseControl_v8d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 800):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
