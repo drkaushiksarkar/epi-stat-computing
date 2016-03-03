125cb2997556533185d881b46d0528a176651214
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 23:57:41 +0000

    fix(config): time_series config v8

--- src/config/time_series_v8_d4644_c43.py
+++ src/config/time_series_v8_d4644_c43.py
@@ -1,10 +1,10 @@
-"""TimeSeries config v8d4644y2015."""
+"""TimeSeries config v8d4644y2016."""
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
 
 @dataclass
-class TimeSeriesConfig_v8d4644y2015:
+class TimeSeriesConfig_v8d4644y2016:
     enabled: bool = True
     batch_size: int = 256
     hidden_dim: int = 512
@@ -14,7 +14,7 @@ class TimeSeriesConfig_v8d4644y2015:
     max_epochs: int = 80
 
     @classmethod
-    def from_dict(cls, d: Dict[str, Any]) -> "TimeSeriesConfig_v8d4644y2015":
+    def from_dict(cls, d: Dict[str, Any]) -> "TimeSeriesConfig_v8d4644y2016":
         return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
 
     def validate(self) -> bool:
