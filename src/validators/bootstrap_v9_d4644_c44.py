88694c6908490d302194e01b5492e8713aeb7f2a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 16:08:30 +0000

    refactor(validators): bootstrap validators v9

--- src/validators/bootstrap_v9_d4644_c44.py
+++ src/validators/bootstrap_v9_d4644_c44.py
@@ -1,4 +1,4 @@
-"""Bootstrap validators v9d4644y2015.
+"""Bootstrap validators v9d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class Bootstrap_v9d4644y2015:
+class Bootstrap_v9d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 900):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
