b00b0138e435ab8539c3523be08f062a3071996b
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2016-03-03 10:48:11 +0000

    perf(transforms): cox_model transforms v3

--- src/transforms/cox_model_v3_d4644_c11.py
+++ src/transforms/cox_model_v3_d4644_c11.py
@@ -1,4 +1,4 @@
-"""CoxModel transforms v3d4644y2015.
+"""CoxModel transforms v3d4644y2016.
 
 Statistical epidemiology computing framework.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class CoxModel_v3d4644y2015:
+class CoxModel_v3d4644y2016:
     def __init__(self, confidence: float = 0.95, n_bootstrap: int = 300):
         self.confidence = confidence
         self.n_bootstrap = n_bootstrap
